4c0463b5d103e355498cc0b585cfdb35d48782d9
Luke Storti <lukestorti@gmail.com>
2025-10-03 10:23:23 +0200

    playground for practicing graphQL

---
+++ schema.js
@@ -0,0 +1,52 @@
+export const typeDefs = `#graphql
+type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
+}
+type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+    game: Game!
+    author: Author!
+}
+type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]
+}
+type Query {
+    reviews: [Review]
+    review(id: ID!): Review
+    games: [Game]
+    game(id: ID!) : Game
+    authors: [Author]
+    author(id:ID!): Author
+}
+type Mutation{
+    deleteGame(id:ID!): [Game]
+    addGame(game:AddGameInput!): Game
+    updateGame(id:ID!,edits: EditGameInput!): Game
+    addReview(review:AddReviewInput!): Review
+}
+
+input AddGameInput{
+    title:String!
+    platform: [String!]!
+}
+
+input EditGameInput{
+title:String
+platform: [String!]
+}
+
+input AddReviewInput{
+    rating: Int!
+    content: String!
+    author_id: String!
+    game_id: String!
+}
+`;
